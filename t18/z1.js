a2c8de492167bebaa45e0cdf660c3a5cf7e89990
slsasha <74455808+slsasha@users.noreply.github.com>
2022-06-11 15:53:09 +0300

    Add files via upload

---
+++ t18/z1.js
@@ -0,0 +1,70 @@
+const users = [
+	{
+		name: "Олег",
+		email: "olegivanov@.gmail.com"
+	},
+	{
+		name: "Таня",
+		email: "taniavit@.gmail.com"
+	},
+	{
+		name: "Тарас",
+		email: "tarik95@.gmail.com"
+	},
+	{
+		name: "Оксана",
+		email: "oksana2002@.gmail.com"
+	}
+];
+function showError(text){
+    let message = document.getElementById(`message`);
+    let message_text = `<div class="alert alert-danger col-6">${text}</div>`;
+
+
+    message.innerHTML = message_text;
+    setTimeout(function(){
+
+        message.innerHTML = ``;
+
+    }, 3000)
+}
+showError(`Повітряна тривога`)
+
+function  showSuccess(text){
+    let message = document.getElementById(`message`);
+    let message_text = `<div class="alert alert-success col-6">${text}</div>`;
+
+
+    message.innerHTML = message_text;
+    setTimeout(function(){
+
+        message.innerHTML = ``;
+
+    }, 3000)
+}
+showSuccess(`Успіх`)
+
+function  checkName(){
+    let name = document.getElementById(`name`).value 
+    let new_arr = users.filter(function(user){
+       return user.name.toLowerCase() == name.toLowerCase()
+        
+    })
+if (new_arr.length > 0){
+    showError(`Користувач з таким ім'ям вже зареєстрований`)
+
+}
+}
+
+function  checkEmail(){
+    let email = document.getElementById(`email`).value 
+    let new_arr = users.filter(function(user){
+       return user.email == email
+        
+    })
+if (new_arr.length > 0){
+    showError(`Користувач з таким email вже зареєстрований`)
+
+}
+}
+
